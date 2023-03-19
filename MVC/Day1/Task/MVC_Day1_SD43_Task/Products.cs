2d912e62fd14804fb8f25b1d3e1ac80e8fe3a80d
AhmedTaha475 <76524115+AhmedTaha475@users.noreply.github.com>
2023-03-19 09:53:18 +0200

    MVC_Day1

---
+++ MVC/Day1/Task/MVC_Day1_SD43_Task/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Day1_SD43_Task
+{
+    public class Products
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+        public Products(int _id, string _name)
+        {
+            
+              this.id = _id;
+            this.name = _name;
+        }
+    }
+}
